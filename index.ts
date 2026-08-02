183c19fe350eeea64a690db099f8b2c5ce562f00
P1xel1ze <170705439+P1xel1ze@users.noreply.github.com>
2026-08-02 18:57:28 +1000

    Add try/catches for safety

--- index.ts
+++ index.ts
@@ -34,32 +34,42 @@ client.on('clientReady', async () => {
 });
 
 client.on('interactionCreate', async act => {
-	if (!act.isChatInputCommand()) return;
-	if (act.commandName === 'imprison') {
-		prisoners.push(act.options.getUser('evildoer', true).id as never)
-		writeFileSync('prisoners.json', JSON.stringify(prisoners))
-		for (let i of prisoners) {
-			(await act.guild?.members.fetch(i))!.roles.set([process.env.PRISONER_ROLE_ID!])
-			for (const channel of (await act.guild?.channels.fetch()!).values()) {
-				if (!channel || !channel?.isTextBased()) continue;
-				const fetchedMsgs = await channel.messages.fetch({ limit: 100 });
-				await channel.bulkDelete(fetchedMsgs.filter(m => m.author.id === i));
+	try {
+		if (!act.isChatInputCommand()) return;
+		if (act.commandName === 'imprison') {
+			prisoners.push(act.options.getUser('evildoer', true).id as never)
+			writeFileSync('prisoners.json', JSON.stringify(prisoners))
+			for (let i of prisoners) {
+				try {
+					(await act.guild?.members.fetch(i))!.roles.set([process.env.PRISONER_ROLE_ID!])
+					for (const channel of (await act.guild?.channels.fetch()!).values()) {
+						if (!channel || !channel?.isTextBased()) continue;
+						const fetchedMsgs = await channel.messages.fetch({ limit: 100 });
+						try {
+							await channel.bulkDelete(fetchedMsgs.filter(m => m.author.id === i));
+						} catch {
+							await act.reply({ content: 'Messages older than 14 days, did not delete', flags: Discord.MessageFlags.Ephemeral })
+						}
+					}
+				} catch {
+					await act.reply({ content: 'An error occurred', flags: Discord.MessageFlags.Ephemeral })
+				}
 			}
+			await act.reply({ content: 'Sent evildoer to prison', flags: Discord.MessageFlags.Ephemeral });
+		} else if (act.commandName === 'release') {
+			let prisoner: Discord.User = act.options.getUser('prisoner', true);
+			(await act.guild?.members.fetch(prisoner))!.roles.set([process.env.REG_USR_ROLE_ID!])
+			let index = prisoners.indexOf(prisoner.id as never)
+			if (index !== -1) prisoners.splice(index, 1);
+			writeFileSync('prisoners.json', JSON.stringify(prisoners))
+		} else if (act.commandName === 'prisoners') {
+			let list: string[] = [];
+			for (let i of prisoners) {
+				list.push(((await client.users.fetch(i)).username))
+			}
+			await act.reply({ content: '@' + list.join(', @'), flags: Discord.MessageFlags.Ephemeral })
 		}
-		await act.reply({ content: 'Sent evildoer to prison', flags: Discord.MessageFlags.Ephemeral });
-	} else if (act.commandName === 'release') {
-		let prisoner: Discord.User = act.options.getUser('prisoner', true);
-		(await act.guild?.members.fetch(prisoner))!.roles.set([process.env.REG_USR_ROLE_ID!])
-		let index = prisoners.indexOf(prisoner.id)
-		if (index !== -1) prisoners.splice(index, 1);
-		writeFileSync('prisoners.json', JSON.stringify(prisoners))
-	} else if (act.commandName === 'prisoners') {
-		let list: string[] = [];
-		for (let i of prisoners) {
-			list.push(((await client.users.fetch(i)).username))
-		}
-		await act.reply({ content: '@' + list.join(', @'), flags: Discord.MessageFlags.Ephemeral })
-	}
+	} catch (e) {logError(e)}
 });
 
 client.login(process.env.BOT_TOKEN)
