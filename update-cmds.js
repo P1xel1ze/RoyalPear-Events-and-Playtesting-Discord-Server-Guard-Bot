cfe6b236cce96231363913999f4d12a31bbe7c7f
P1xel1ze <170705439+P1xel1ze@users.noreply.github.com>
2026-08-30 12:51:53 +1000

    Restrict commands to only users who can ban other users

--- update-cmds.js
+++ update-cmds.js
@@ -5,7 +5,7 @@ const cmds = [
 	new SlashCommandBuilder().setName('imprison').setDescription('Sends a user to prison').addUserOption(o=>o.setName('evildoer').setDescription('The naughty person to imprison').setRequired(true)),
 	new SlashCommandBuilder().setName('prisoners').setDescription('Lists users that have been imprisoned'),
 	new SlashCommandBuilder().setName('release').setDescription('Releases a user from prison').addUserOption(o=>o.setName('prisoner').setDescription('The person to release from prison').setRequired(true)),
-].map(c=>{return c.toJSON()})
+].map(c=>{return c.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers).toJSON()})
 
 console.log(chalk.yellow.bold('Registering slash commands...'));
 await new REST({ version: '10' }).setToken(process.env.BOT_TOKEN).put(Routes.applicationCommands(process.env.APPLICATION_ID), { body: cmds });
