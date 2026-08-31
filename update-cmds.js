5f4ac09da35eb7274ab42a99ccfced7fdbde710a
P1xel1ze <170705439+P1xel1ze@users.noreply.github.com>
2026-08-31 18:01:29 +1000

    Fix update-cmds.js to use util.styleText() instead of Chalk
    Idk how I forgot to update this file...

--- update-cmds.js
+++ update-cmds.js
@@ -1,5 +1,5 @@
 import { REST, SlashCommandBuilder, PermissionFlagsBits, Routes } from 'discord.js';
-import chalk from 'chalk';
+import { styleText } from 'node:util';
 
 const cmds = [
 	new SlashCommandBuilder().setName('imprison').setDescription('Sends a user to prison').addUserOption(o=>o.setName('evildoer').setDescription('The naughty person to imprison').setRequired(true)),
@@ -7,6 +7,6 @@ const cmds = [
 	new SlashCommandBuilder().setName('release').setDescription('Releases a user from prison').addUserOption(o=>o.setName('prisoner').setDescription('The person to release from prison').setRequired(true)),
 ].map(c=>{return c.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers).toJSON()})
 
-console.log(chalk.yellow.bold('Registering slash commands...'));
+console.log(styleText(['yellow', 'bold'], 'Registering slash commands...'));
 await new REST({ version: '10' }).setToken(process.env.BOT_TOKEN).put(Routes.applicationCommands(process.env.APPLICATION_ID), { body: cmds });
-console.log(chalk.green.bold('Slash commands registered!'));
+console.log(styleText(['green', 'bold'], 'Slash commands registered!'));
